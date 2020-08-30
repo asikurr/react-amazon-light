e27ac20c0d0dacefd2811e46798bb5aebfab15fc
Asikur <asikurrahman7@gmail.com>
2020-08-30 15:30:40 +0600

    working on review page.

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,15 +1,17 @@
 import React, {useEffect,useState} from 'react';
 import { getDatabaseCart } from '../../utilities/databaseManager';
 import fakeData from '../../fakeData';
+import ReviewItems from '../reviewItems/ReviewItems';
 
 const Review = () => {
     const [cart, setCart] = useState([]);
+        console.log(cart)
     useEffect(() => {
         const saveData = getDatabaseCart();
         // console.log(saveData)
-        const dataKey = Object.keys(saveData);
+        const dataKeys = Object.keys(saveData);
         //  console.log(dataKey)
-        const productData = dataKey.map(key =>{
+        const productData = dataKeys.map(key =>{
             const product = fakeData.find(pd => pd.key === key)
             // console.log(product)
             product.quantity = saveData[key]
@@ -17,15 +19,14 @@ const Review = () => {
 
         });
         setCart(productData)
-            // saveData[key]
-        console.log(cart)
-            // const producdkey = dataKey.find(orderKey => orderKey === key);
-            // console.log(producdkey)
-
     }, [])
     return (
         <div>
-        <h1>This is Review Page.</h1>
+        <h1>This is Review Page. {cart.length}</h1>
+        {
+            cart.map(pd => <ReviewItems key={pd.key} product = {pd}/>)
+        }
+        
         </div>
     );
 };
---
+++ src/components/reviewItems/ReviewItems.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+
+const ReviewItems = (props) => {
+    // console.log(props)
+    const { name, quantity, price, img } = props.product;
+    return (
+        <Fragment>
+
+            <div className="shop-container">
+                <div className="product__img">
+                    <img src={img} alt="product img" />
+                </div>
+
+                <div className="product__content">
+                    <h3 className="product__heading">{name}</h3>
+                    <p>Price : ${price}</p>
+                    <p>Quantity:  {quantity} </p>
+                    <button className="product__btn">Remove</button>
+                </div>
+            </div>
+
+        </Fragment>
+
+    );
+};
+
+export default ReviewItems;
